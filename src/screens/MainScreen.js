c3d3692a9f55d185339f73950606e0538961bf0b
rayquezia03 <rayquezia03@gmail.com>
2022-09-10 11:10:10 -0300

    p2

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,14 +1,31 @@
-import { View, StyleSheet } from "react-native";
+import {React, useState} from 'react';
+import { View, StyleSheet} from "react-native";
 import Topo from "../components/Topo";
 import Painel from "../components/Painel";
+import Painel2 from "../components/painel2";
 import Personagens from "../components/personagens";
+//import Score from "../components/score";
+
+const messages = {
+    computerWin: {
+        title: "Derrota",
+        message: "Você perdeu!",
+      },
+    playerWin: {
+        title: "Vitória!",
+        message: "Você venceu!",
+      },
+    };
 
 export default function MainScreen(){
+    const [messageModal, setMessageModal] = useState("");
+
     return(
         <View style={styles.container}>
             <Topo/>
             <Painel/>
             <Personagens/>
+            <Painel2/>
         </View>
     );
 }
